a3e3b2ce649fe3daf36d805062f6b7a1eebd5cbd
tadeo14 <tadeoacosta14@gmail.com>
2023-10-11 02:17:13 -0300

    primeros pasos

---
+++ prueba.js
@@ -0,0 +1,9 @@
+var nombre;
+var edad;
+var categoria;
+
+nombre = prompt ("Coloca tu nombre: ");
+edad=prompt("¿Cuál es tu edad?");
+categoria=confirm("dime tu categoria: )");
+
+document.write ("tu nombre es " + nombre + "la edad que tiene es " + "y tu categoria es")
