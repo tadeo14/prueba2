97f9a491879afd437c8b07ee578e0eba523e59c8
tadeo14 <tadeoacosta14@gmail.com>
2023-10-11 02:23:06 -0300

    correcion de la categoria

--- prueba.js
+++ prueba.js
@@ -4,6 +4,6 @@ var categoria;
 
 nombre = prompt ("Coloca tu nombre: ");
 edad=prompt("¿Cuál es tu edad?");
-categoria=confirm("dime tu categoria: )");
+categoria= prompt("dime tu categoria: ");
 
 document.write ("tu nombre es " + nombre + "la edad que tiene es " + "y tu categoria es")
