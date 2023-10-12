41c538e4ca5e01d42eada8e4549da7c2be109313
tadeo14 <tadeoacosta14@gmail.com>
2023-10-12 20:54:11 -0300

    comentamos el ejerciccio anterior, pegue codigo de chat gpt

--- prueba.js
+++ prueba.js
@@ -1,4 +1,4 @@
-var nombre;
+/*var nombre;
 var edad;
 var categoria;
 
@@ -9,3 +9,36 @@ do {
 } while (confirm("desea guardar los datos") == false);
 
 document.write ("tu nombre es " + nombre + "la edad que tiene es " +edad + "y tu categoria es"+ categoria)
+Ejercicio 1
+Se generará un mensaje de alerta que saludará
+al usuario con un mensaje diferente según el día
+de la semana.
+Trabajaremos con getDay() y switch
+
+*/
+var fecha = new Date(); // Crea una instancia de Date que representa la fecha y la hora actual
+var diaDeLaSemana = fecha.getDay();
+
+switch (diaDeLaSemana) {
+    case 0:
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miércoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+}
