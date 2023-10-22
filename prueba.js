67d39cd310876a91c85da17b784d800d7de3cf8a
tadeo14 <tadeoacosta14@gmail.com>
2023-10-22 02:05:37 -0300

    se realizo el desafio

--- prueba.js
+++ prueba.js
@@ -130,3 +130,79 @@ while (true) {
     }
 }
 */
+var FlujoDeCaja = [
+    {
+        periodo: "Enero",
+        ingresos: 1500,
+        egresos: 1500
+    },
+    {
+        periodo: "Febrero",
+        ingresos: 2500,
+        egresos: 2500
+    },
+    {
+        periodo: "Marzo",
+        ingresos: 84683,
+        egresos: 1155
+    },
+    {
+        periodo: "Abril",
+        ingresos: 135353,
+        egresos: 1533
+    },
+    {
+        periodo: "Mayo",
+        ingresos: 1535,
+        egresos: 5434
+    },
+    {
+        periodo: "Junio",
+        ingresos: 4343354,
+        egresos: 5434534
+    },
+    {
+        periodo: "Julio",
+        ingresos: 435453,
+        egresos: 4543
+    },
+    {
+        periodo: "Agosto",
+        ingresos: 78351,
+        egresos: 7816
+    },
+    {
+        periodo: "Septiembre",
+        ingresos: 1878,
+        egresos: 95634
+    },
+    {
+        periodo: "Octubre",
+        ingresos: 48483,
+        egresos: 9433
+    },
+    {
+        periodo: "Noviembre",
+        ingresos: 35483,
+        egresos: 53133
+    },
+    {
+        periodo: "Diciembre",
+        ingresos: 3843,
+        egresos: 348133
+    },
+]; //se crea el arrray donde permite almacenar datos y luego permite realizar operaciones
+
+var ingresos = 0;
+var egresos = 0;
+
+for (var i=0; i< FlujoDeCaja.length; i++) {
+    var Actual = FlujoDeCaja [i];
+    ingresos+= Actual.ingresos;
+    egresos +=Actual.egresos;
+}
+if(ingresos < egresos) {
+    alert("El flujo de caja genera pérdidas");
+} else {
+    alert("El flujo de caja no genera pérdidas");
+}
